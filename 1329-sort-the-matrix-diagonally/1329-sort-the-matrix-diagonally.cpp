05c112ecaffa7567c1d6429f9753d0a810b3805c
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-28 17:48:34 +0530

    Time: 16 ms (64.04%), Space: 8.7 MB (95.66%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -6,12 +6,12 @@ public:
         int m= mat.size()-1, n= mat[0].size()-1;    // dimensions
         for(int j=0; j<=n;j++)                      // for top right diagonals
         {
-            for(int i=0; i<=min(m,j);i++)    // storing each diagonal
+            for(int i=0; i<=min(m,j);i++)           // storing each diagonal
             {
                 diag.push_back(mat[i][n-j+i]);      // each diagonal element looks like mat[i][m-1-j+i]
             }
             sort(diag.begin(),diag.end());
-            for(int i=0; i<=min(m,j);i++)    // replacing sorted pattern in top right diagonals
+            for(int i=0; i<=min(m,j);i++)           // replacing sorted pattern in top right diagonals
             {
                 mat[i][n-j+i] = diag[i];
             }
@@ -19,12 +19,12 @@ public:
         }
         for(int j=0;j<=m;j++)                       // bottom left diagonals
         {
-            for(int i=0; i<=min(n,j);i++)    // storing each diagonal
+            for(int i=0; i<=min(n,j);i++)           // storing each diagonal
             {
                 diag.push_back(mat[m-j+i][i]);      // diagonal element looks like mat[n-1-j-i][i];
             }
             sort(diag.begin(),diag.end());          // creating a sorted list for diagonal elements
-            for(int i=0; i<=min(n,j);i++)    // replacing sorted pattern in bottom left diagonals
+            for(int i=0; i<=min(n,j);i++)           // replacing sorted pattern in bottom left diagonals
             {
                 mat[m-j+i][i] = diag[i];
             }
