b922003df69869cc5038d81d245dd483d232264d
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-28 18:21:30 +0530

    Time: 23 ms (5.63%), Space: 6.8 MB (20.93%) - LeetHub

---
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int c=0;
+        unordered_map <int,int> m;
+        vector<int> bullidx;
+        for(int i=0; i<secret.size();i++)
+        {
+            if(secret[i]==guess[i])
+            {
+                bullidx.push_back(i);
+            }
+            else
+            {
+                auto it= m.find(secret[i]);
+                if (it==m.end())
+                    m.insert({secret[i],1}); 
+                else
+                    m[secret[i]]++;
+            }
+        }
+        for(int i=0; i<guess.size();i++)
+        {
+            if(!count(bullidx.begin(),bullidx.end(),i))
+            {
+                auto it= m.find(guess[i]);
+                if (it!=m.end() && it->second-->0)
+                {
+                    c++;
+                }
+            }
+        }
+        string answer= to_string(bullidx.size())+ "A" + to_string(c) + "B";
+        return answer;
+    }
+};
+
