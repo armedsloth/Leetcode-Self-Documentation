08d123a885a85332dc0786d887a58aec43249a49
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-29 21:02:19 +0530

    Time: 8 ms (6.65%), Space: 6.3 MB (45.60%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int j,k=0,p=0;
-        for(j=0;j <s.size();j++)
-            if(s[j]!='#')
-                s[k++]=s[j];
+        int j,k=0,p=0;              // j as fast pointer, k and p as true size
+        for(j=0;j <s.size();j++)    
+            if(s[j]!='#')           
+                s[k++]=s[j];        // storing legal character at k position and incrementing k
             else
-                k=max(0,k-1);
+                k=max(0,k-1);       // removing top character if it is there
         for(j=0;j<t.size();j++)
-            if(t[j]!='#')
+            if(t[j]!='#')           
                 t[p++]=t[j];
             else
                 p=max(0,p-1);
 
-        s=s.substr(0,k);
+        s=s.substr(0,k);            // resizing string to the useful size of characters left.
         t=t.substr(0,p);
         return s==t;
     }
