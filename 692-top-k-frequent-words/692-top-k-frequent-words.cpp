eb8c952c2583cc50313f7bf6365adfbf69dbc9e0
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-30 14:04:57 +0530

    Time: 15 ms (80.61%), Space: 12.8 MB (36.36%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     
-    struct comp
-    {
-        bool operator()(const pair<string,int> & l, const pair <string, int> & r) const
+    struct comp                                     // comparison function for sorting by value in a pair
+    {                                               // sorts in non-increasing then lexicographical order
+        bool operator()(const pair<string,int> & l, const pair <string, int> & r) const     
         {
-            if (l.second != r.second)
-                return l.second > r.second;
-            return l.first < r.first;
+            if (l.second != r.second)               // comparing frequency
+                return l.second > r.second;         
+            return l.first < r.first;               // comparing lexical order
         }
     };
     
     vector<string> topKFrequent(vector<string>& words, int k) {
-        map <string, int> um;
-        vector<string> ans;
-        for(int i=0;i<words.size();i++)
+        map <string, int> um;                       // frequency map for strings
+        for(int i=0;i<words.size();i++)             //storing frequencies of strings in the map
         {
             auto it =um.find(words[i]);
             if(it==um.end())
@@ -22,11 +21,13 @@ public:
             else
                 it->second++;   
         }
-        set<pair<string,int>, comp> pq (um.begin(),um.end());
-        auto it = pq.begin();
+        // now store the strings uniquely in a set accoring to sort function prepared
+        set<pair<string,int>, comp> s (um.begin(),um.end());    
+        auto it = s.begin();                        // iterator for pushing
+        vector <string> ans;                        //output vector
         for(int i=0; i<k;i++)
-        {
-            ans.push_back(it->first);
+        {   
+            ans.push_back(it->first);               // pushing k strings
             it++;
         }
         return ans;
