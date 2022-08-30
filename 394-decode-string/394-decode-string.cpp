4904944d2822e4dfa20f316a7010342725cf2417
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-30 12:33:35 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (88.11%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -2,37 +2,42 @@ class Solution {
 public:
     string findmini(string s, int posi)
     {
-        int open=0 , size=s.size();
-        for(int i=posi; i<size;i++)
+        int size=s.size(), open=0;
+        for(int i=posi;i<size;i++)
         {
             if(s[i]=='[')
                 open++;
             if(s[i]==']')
                 open--;
             if(!open)
-                return s.substr(posi+1,i-posi);
+               return s.substr(posi+1,i-posi-1); 
         }
-        return "" ;
-    }
+        return "";
+    }    
+    
     
     string decodeString(string s) {
+        int size=s.size(), num=0;
         string ans;
-        int num=0;
-        for(int i=0; i<s.size();i++)
+        for(int i=0;i<size;i++)
         {
-            if(isdigit(s[i]))
-            {
-                num= num*10 + (s[i]-'0');
-                if(!isdigit(s[i+1]))
-                {
-                    for(int j=0; j< num-1 ;j++ )
-                        ans+=decodeString(findmini(s,i+1));
-                    num=0;
-                }
+          if(isdigit(s[i]))
+          {
+            num= num*10 + s[i]-'0';
+            if(s[i+1]=='[')
+            {   
+                for(int j=0;j<num-1;j++)
+                    ans+=decodeString(findmini(s,i+1));
+                num=0;
             }
-            else
-                if(isalpha(s[i]))
-                    ans+=s[i];
+          }
+          else
+          {
+              if(isalpha(s[i]))
+              {
+                  ans+=s[i];
+              }
+          }
         }
         return ans;
     }
