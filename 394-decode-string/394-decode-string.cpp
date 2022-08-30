596c494a9ea0a858b1fcfba7fdeae73e8ab1ab97
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-30 12:45:44 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (56.21%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    string findmini(string s, int posi)
+    string findmini(string s, int posi)             // function to find the string to be repeated num times
     {
         int size=s.size(), open=0;
-        for(int i=posi;i<size;i++)
+        for(int i=posi;i<size;i++)                  // loop catching when the brackets balance
         {
             if(s[i]=='[')
                 open++;
             if(s[i]==']')
                 open--;
             if(!open)
-               return s.substr(posi+1,i-posi-1); 
+               return s.substr(posi+1,i-posi-1);    // returning the interior of outermost brackets
         }
         return "";
     }    
@@ -19,21 +19,21 @@ public:
     string decodeString(string s) {
         int size=s.size(), num=0;
         string ans;
-        for(int i=0;i<size;i++)
+        for(int i=0;i<size;i++)                             // for every character
         {
-          if(isdigit(s[i]))
+          if(isdigit(s[i]))                                 // number encountered
           {
-            num= num*10 + s[i]-'0';
+            num= num*10 + s[i]-'0';                         // getting the complete number in num 
             if(s[i+1]=='[')
             {   
-                for(int j=0;j<num-1;j++)
-                    ans+=decodeString(findmini(s,i+1));
-                num=0;
+                for(int j=0;j<num-1;j++)                    // getting the inner string into the ans num-1 times
+                    ans+=decodeString(findmini(s,i+1));     // finding the inner string, and decoding that before adding
+                num=0;                                      // reseting num for future use
             }
           }
           else
           {
-              if(isalpha(s[i]))
+              if(isalpha(s[i]))                             // alphabets going into ans directly
               {
                   ans+=s[i];
               }
