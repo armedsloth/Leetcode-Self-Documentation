a1fc42b1a0a8d3234b371bcb47d331b2804082d2
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-30 13:46:46 +0530

    Time: 16 ms (75.74%), Space: 12.7 MB (36.36%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    struct comp
+    {
+        template <typename T>
+        bool operator()(const T& l, const T& r) const
+        {
+            if (l.second != r.second)
+                return l.second > r.second;
+            return l.first < r.first;
+        }
+    };
+    
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map <string, int> um;
+        vector<string> ans;
+        for(int i=0;i<words.size();i++)
+        {
+            auto it =um.find(words[i]);
+            if(it==um.end())
+                um.insert({words[i],1});
+            else
+                it->second++;   
+        }
+        set<pair<string,int>, comp> pq (um.begin(),um.end());
+        auto it = pq.begin();
+        for(int i=0; i<k;i++)
+        {
+            ans.push_back(it->first);
+            it++;
+        }
+        return ans;
+    }
+};
