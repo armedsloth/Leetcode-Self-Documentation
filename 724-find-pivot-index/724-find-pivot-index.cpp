a92cebe0ff6e63f7a0aba3750919081a54781bcd
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-08-31 13:55:57 +0530

    Time: 63 ms (10.91%), Space: 31.1 MB (52.31%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        partial_sum(nums.begin(),nums.end(),nums.begin());  // store in place the partial sums till each element
-        for(int i=0; i<nums.size();i++)                     // going left to right to find first pivot
-        {
-            //leftsum is store in nums[i-1] for non zero i
-            int leftsum= i==0 ? 0: nums[i-1] ;
-            
-            //rightsum is calculated as (sum till last element) - (sum till i)
-            int rightsum = i==(nums.size()-1)? 0: (nums[nums.size()-1]-nums[i]);
-            
-            if(leftsum==rightsum)      
-                return i;                                   // returning index
+        int sum=0, yet=0;
+        for( int it : nums) sum+=it;      //finding total sum of the array
+        
+        for(int i=0 ; i<nums.size(); i++)        // left to right traversing
+        {  
+            if(sum - 2*yet == nums[i])   // for pivot, total = left + right + pivot = 2*left + pivot
+                return i;
+            yet+=nums[i];                 // if not pivot, then update sum found yet.
         }
-        return -1;                                          // never found
+        return -1;                        // never found
     }
 };
