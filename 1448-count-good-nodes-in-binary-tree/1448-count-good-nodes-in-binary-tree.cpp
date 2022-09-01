c85e6abbf11b652fb70efd0315d8e2cea8f7a630
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-09-01 23:21:05 +0530

    Time: 262 ms (14.48%), Space: 86.3 MB (91.25%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,20 +11,20 @@
  */
 class Solution {
 public:
-    void mydfs( TreeNode* cur, int &count, int maxyet)
+    void mypre( TreeNode* cur, int &count, int maxyet)  // preorder traversal to recursively keep track of path
     {
-        if( !cur )
+        if( !cur )                                      // node doesn't exist
             return;
-        if( cur->val>=maxyet)
+        if( cur->val>=maxyet)                           // is a good node
             count++;
-        maxyet=max(cur->val, maxyet);
-        mydfs(cur->left, count, maxyet);
-        mydfs(cur->right, count, maxyet);
+        maxyet=max(cur->val, maxyet);                   
+        mypre(cur->left, count, maxyet);                // branch into left and right sub tree preorder traversal
+        mypre(cur->right, count, maxyet);
         
     }
     int goodNodes(TreeNode* root) {
-        int count=0;
-        mydfs(root, count, INT_MIN);
+        int count=0;                                    // counter for good nodes
+        mypre(root, count, INT_MIN);
         return count;
     }
 };
