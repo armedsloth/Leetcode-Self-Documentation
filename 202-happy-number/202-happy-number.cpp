3cd5cdfe0580430a75306cfb0b68a63a081373f6
armedsloth <73989223+armedsloth@users.noreply.github.com>
2022-09-19 19:49:18 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (6.61%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     bool isHappy(int n) {
         set <int> rec;
+        int sum;
         while(1)
         {
             if(rec.find(n)!=rec.end())
                 return false;
             rec.insert(n);
-            int sum=0;
+            sum=0;
             while(n>0)
             {
                 sum+=(n%10) * (n%10);
                 n/=10;
             }
-            if(sum==1)
-                return true;
-            
+            if(sum==1)  return true;
             n=sum;                    
         }
         return false;
